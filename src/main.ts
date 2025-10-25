f9dd60700bfbe4bfeeaa3b0f07cc75b6179ff92e
andrei <andrei.ghita2002@gmail.com>
2025-10-25 20:19:29 +0100

    idk what this is

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import {Application, Ticker} from 'pixi.js';
 import { Plane } from "./Plane.ts";
 import {Airport} from "./Airport.ts";
+import {Map} from "./Map.ts";
 import {airportLocations} from "./Airport-locations.ts";
 
 // every how many frame(-ish) to spawn a new plane in
@@ -12,6 +13,8 @@ class Main {
   app: Application;
   planes: Plane[] = [];
   airports: Airport[] =[];
+  // @ts-ignore
+  map : Map; // shows error cause not initialised. Don't care :)
   new_plane_cum = 0;
 
   constructor() {
@@ -22,6 +25,7 @@ class Main {
     // Preload assets
     await Plane.preload();
     await Airport.preload();
+    await Map.preload();
 
     const width = window.innerWidth;
     const height = window.innerHeight * 0.9;
@@ -35,10 +39,12 @@ class Main {
     document.body.appendChild(this.app.canvas);
     this.addPlane()
     this.addAirport()
+    this.addMap()
 
     // Add an animation loop callback to the application's ticker.
     this.app.ticker.add(t => this.mainLoop(t));
 
+    // Move elements to accommodate for resize
     window.addEventListener("resize", () => {this.resize(window.innerWidth, (window.innerHeight * 0.9))});
   }
 
@@ -54,8 +60,9 @@ class Main {
       }
     }
 
-    // Plane update
-    this.planes.forEach((p) => p.update(time));
+    // Plane update and deletions
+    this.planes.forEach((plane) => plane.update(time))
+    this.planes = this.planes.filter((p) => !p.to_delete);
   }
 
   addPlane() {
@@ -65,6 +72,7 @@ class Main {
     // add to stage.
     this.app.stage.addChild(plane);
   }
+
   addAirport(){
     airportLocations(this.app.screen.width,this.app.screen.height).forEach((coord ) =>{
       let airport = new Airport(coord[0],coord[1], 0.5);
@@ -73,8 +81,15 @@ class Main {
     })
   }
 
+  addMap(){
+    let map = new Map(0,0,this.app.screen.width,this.app.screen.height);
+    this.map = map;
+    this.app.stage.addChildAt(map,0);
+  }
+
   // todo call this from somewhere
   resize(new_width: number, new_height: number) {
+    //this.planes.forEach((p) => p.resize(new_width, new_height));
     const coords = airportLocations(new_width,new_height);
     this.airports.forEach((value, index) => value.position.set(coords[index][0], coords[index][1]));
   }
@@ -84,5 +99,5 @@ class Main {
 (async () => {
   // Create a PixiJS application.
   let main = new Main();
-  main.run();
+  await main.run();
 })();
