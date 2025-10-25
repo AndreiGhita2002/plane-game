6eca23a893da7d57877b59dff90e21542efbb7e6
Fraser Miller <frasermiller0@gmail.com>
2025-10-25 18:30:15 +0100

    added map resize

--- src/Airport.ts
+++ src/Airport.ts
@@ -4,12 +4,13 @@ export class Airport extends Sprite{
 
     static airport_texture : Texture;
     static async preload(){
-        Airport.airport_texture= await Assets.load({src : "sprite/airport.svg"});
+        Airport.airport_texture = await Assets.load("sprite/airport.svg");
     }
     constructor(x: number, y: number, scale:number){
        super(Airport.airport_texture);
        this.scale.set(scale)
        this.position.set(x,y)
+
     }
 
 }
