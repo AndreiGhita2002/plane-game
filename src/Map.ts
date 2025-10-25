fa99b1170037d5b829f652c07152098953459f4c
Fraser Miller <frasermiller0@gmail.com>
2025-10-25 18:28:42 +0100

    Added background map

---
+++ src/Map.ts
@@ -0,0 +1,16 @@
+import {Texture, Assets, Sprite } from "pixi.js"
+
+export class Map extends Sprite{
+    static mapTexture: Texture;
+
+    static async preload(){
+        Map.mapTexture = await Assets.load("/sprite/Map.svg")
+    }
+
+    constructor(x : number, y : number, width: number, height: number){
+        super(Map.mapTexture);
+        this.position.set(x, y);
+        this.width = width;
+        this.height = height;
+    }
+}
