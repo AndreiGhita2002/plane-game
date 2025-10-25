3d6baa674edb4f0e682a45f5a6cfc121c2919d3f
Fraser Miller <frasermiller0@gmail.com>
2025-10-25 19:13:20 +0100

    aaahahah

--- src/main.ts
+++ src/main.ts
@@ -90,7 +90,6 @@ class Main {
     const coords = airportLocations(new_width,new_height);
     this.airports.forEach((value, index) => value.position.set(coords[index][0], coords[index][1]));
   }
-
 }
 
 // Asynchronous IIFE
