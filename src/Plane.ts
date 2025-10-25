2dd931c58cfeb72fbdadb120998fad093b5a070a
andrei <andrei.ghita2002@gmail.com>
2025-10-25 16:44:23 +0100

    some shit

--- src/Plane.ts
+++ src/Plane.ts
@@ -4,6 +4,8 @@ import {getRandomInt} from "./util/random.ts";
 const PLANE_URL = 'https://pixijs.com/assets/bunny.png';
 const PLANE_MIN_SPAWN_SPEED = 1;
 const PLANE_MAX_SPAWN_SPEED = 5;
+const MAGIC_NUMBER = 1;
+const SIN_INCREMENT = Math.PI / 100;
 
 export class Plane extends Sprite {
   static plane_texture: Texture;
@@ -13,7 +15,14 @@ export class Plane extends Sprite {
   max_x: number;
   max_y: number;
 
+  speed: number;
   velocity: [number, number];
+
+  // Pathfinding information
+  pathfind_mode: number; // 0 - initial, 1 - player set
+  // for pathfind_mode==1:
+  goal: [number, number];
+  turning: boolean = false;
 
   static async preload() {
     // load asset
@@ -21,16 +30,41 @@ export class Plane extends Sprite {
   }
 
   update(time: Ticker) {
-    // if it is time to pathfind:
-    // this.pathfind()
+    if (this.pathfind_mode == 0) {
+      // plane is on its initial path; player has not touched it yet
 
+      // delete the plane if out of bounds
+      if (this.x < 0) this.to_delete = true;
+      if (this.y < 0) this.to_delete = true;
+      if (this.x > this.max_x) this.to_delete = true;
+      if (this.y > this.max_x) this.to_delete = true;
+      if (this.to_delete) return;
+
+      // calculate next position on the sine wave
+      // todo sin wave motion of planes
+      // let v = [
+      //   Math.cos()
+      // ]
+      // this.sin_i += SIN_INCREMENT;
+    } else {
+      // player has chosen a destination
+      if (this.turning) {
+
+      }
+      // else: no course correction needs to be done
+
+      //todo if Plane is over Airport code here
+    }
+
+    // update position
     this.x += this.velocity[0] * time.deltaTime;
     this.y += this.velocity[1] * time.deltaTime;
+  }
 
-    if (this.x < 0) this.to_delete = true;
-    if (this.y < 0) this.to_delete = true;
-    if (this.x > this.max_x) this.to_delete = true;
-    if (this.y > this.max_x) this.to_delete = true;
+  change_path(goal_x: number, goal_y: number) {
+    this.goal = [goal_x, goal_y];
+    this.pathfind_mode = 1;
+    this.turning = true;
   }
 
   resize(new_width: number, new_height: number) {
@@ -46,7 +80,7 @@ export class Plane extends Sprite {
 
     this.velocity = [0, 0];
 
-    let vel = getRandomInt(PLANE_MIN_SPAWN_SPEED, PLANE_MAX_SPAWN_SPEED);
+    this.speed = getRandomInt(PLANE_MIN_SPAWN_SPEED, PLANE_MAX_SPAWN_SPEED);
     let angle = Math.random() * Math.PI;
 
     // clamp angle
@@ -61,8 +95,7 @@ export class Plane extends Sprite {
       this.x = getRandomInt(base, max_x - base);
       this.y = 0;
 
-      this.velocity = [Math.cos(angle) * vel, Math.sin(angle) * vel];
-
+      this.velocity = [Math.cos(angle) * this.speed, Math.sin(angle) * this.speed];
     }
     if (edge == 1) {
       // east
@@ -70,7 +103,7 @@ export class Plane extends Sprite {
       this.x = max_x
       this.y = getRandomInt(base, max_y - base);
 
-      this.velocity = [-Math.sin(angle) * vel, Math.cos(angle) * vel];
+      this.velocity = [-Math.sin(angle) * this.speed, Math.cos(angle) * this.speed];
     }
     if (edge == 2) {
       // south
@@ -78,7 +111,7 @@ export class Plane extends Sprite {
       this.x = getRandomInt(base, max_x - base);
       this.y = max_y;
 
-      this.velocity = [Math.cos(angle) * vel, -Math.sin(angle) * vel];
+      this.velocity = [Math.cos(angle) * this.speed, -Math.sin(angle) * this.speed];
     }
     if (edge == 3) {
       // west
@@ -86,20 +119,10 @@ export class Plane extends Sprite {
       this.x = 0;
       this.y = getRandomInt(base, max_y - base);
 
-      this.velocity = [Math.sin(angle) * vel, Math.cos(angle) * vel];
+      this.velocity = [Math.sin(angle) * this.speed, Math.cos(angle) * this.speed];
     }
+    this.pathfind_mode = 0;
+    this.overall_direction = this.velocity
+    this.goal = [0, 0]
   }
-
-  // Pathfinding information
-  // pathfind_mode: number; // 0 - initial, 1 - player set
-  // overall_direction: [number, number]
-  // goal: [number, number]
-  //
-  // pathfind() {
-  //   if (this.pathfind_mode == 0) {
-  //     // initial edge-to-edge travel
-  //   } else {
-  //     // player set travel
-  //   }
-  // }
 }
