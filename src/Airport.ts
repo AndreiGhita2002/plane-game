b7e071c999bd33c205118b11792de4144f853fdf
Fraser Miller <frasermiller0@gmail.com>
2025-10-25 14:44:11 +0100

    added airports

--- src/Airport.ts
+++ src/Airport.ts
@@ -1,11 +1,15 @@
 import {Assets, Sprite, Texture} from 'pixi.js';
 
-class Airport extends Sprite{
+export class Airport extends Sprite{
 
-    static airportTexture : Texture;
+    static airport_texture : Texture;
+    static async preload(){
+        Airport.airport_texture = await Assets.load("~/sprite/airport.svg");
+    }
+    constructor(x: number, y: number){
+       super(Airport.airport_texture);
+       this.position.set(x,y)
 
-    constructor(){
-       super(Airport.airportTexture);
     }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { Application } from 'pixi.js';
 import { Plane } from "./Plane.ts";
+import {Airport} from "./Airport.ts";
 
 class Main {
   app: Application;
   planes: Plane[] = [];
+  airports: Airport[] =[];
 
   constructor() {
     this.app = new Application()
@@ -12,6 +14,7 @@ class Main {
   async run() {
     // Preload assets
     await Plane.preload();
+    await Airport.preload();
 
     // Initialize the application.
     await this.app.init({ background: '#1099bb', resizeTo: window });
@@ -20,6 +23,7 @@ class Main {
     document.body.appendChild(this.app.canvas);
 
     this.addPlane()
+    //this.addAirport()
 
     // Add an animation loop callback to the application's ticker.
     this.app.ticker.add((time) => {
@@ -34,7 +38,18 @@ class Main {
     // add to stage.
     this.app.stage.addChild(plane);
   }
+  addAirport(){
+    const screen_bounds_max : number[] = [this.app.screen.width, this.app.screen.height];
+    const screen_bounds_min : number[] = [0,0]
+    const getRandom = (min : number, max : number):number =>{
+      return Math.random() * (max-min) + min;
+    }
 
+    const location : number[] = [getRandom(screen_bounds_min[0], screen_bounds_max[0]), getRandom(screen_bounds_min[1], screen_bounds_max[1])];
+    let airport = new Airport(location[0],location[1]);
+    this.airports.push(airport)
+    this.app.stage.addChild(airport);
+  }
   // todo call this from somewhere
   resize(new_width: number, new_height: number) {
     this.planes.forEach((p) => p.resize(new_width, new_height));
