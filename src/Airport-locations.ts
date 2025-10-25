bd329d237e2aae2ec50725d876c70fc27060785f
Fraser Miller <frasermiller0@gmail.com>
2025-10-25 20:38:13 +0100

    fixed airport locations

--- src/Airport-locations.ts
+++ src/Airport-locations.ts
@@ -10,7 +10,7 @@ export const airportLocations = (screen_width: number, screen_hight: number) => 
         [0.7*screen_width,0.27*screen_hight],
         [0.8*screen_width,0.57*screen_hight],
         [0.87*screen_width,0.75*screen_hight],
-        [1*screen_hight,1*screen_hight],
+        [0.6*screen_width,0.1*screen_hight],
     ];
 }
 
